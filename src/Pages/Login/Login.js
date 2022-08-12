bb4c10307cbaee75c50cbb686c96e967f39bfb4b
Rakibul Islam <rakib245875@gmail.com>
2022-08-13 00:38:41 +0600

    added google login with redirect

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { useEffect } from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [user] = useAuthState(auth);
+    const [signInWithGoogle, googleUser, loading, error] =
+        useSignInWithGoogle(auth);
     const navigate = useNavigate();
     useEffect(() => {
-        if (user) {
+        if (user || googleUser) {
             navigate("/home");
         }
-    }, [user, navigate]);
+    }, [googleUser, user, navigate]);
     if (loading) {
         return <div>Loading...</div>;
-    }
-    if (user) {
-        return navigate("/home");
     }
 
     return (
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -2,15 +2,26 @@ import React from "react";
 import { signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user) {
+            navigate("/login");
+        }
+    }, [user, navigate]);
+    const handleSignOut = () => {
+        signOut(auth);
+    };
     return (
         <header>
             <nav className="border-b border-gray-700">
                 <div className="bg-gray-700 text-white p-3 text-left flex justify-between">
                     <span> user: {user?.displayName}</span>
-                    <button onClick={() => signOut(auth)}>Sign Out</button>
+                    <button onClick={handleSignOut}>Sign Out</button>
                 </div>
                 <ul className="flex justify-center gap-10">
                     <li>
